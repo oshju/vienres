82310418b29ebd4ab0d5974e8c9a9954a9e3b0fa
oscar <dadada>
2021-08-23 13:48:48 +0200

    commit lunes

--- app/src/main/java/com/example/ejemplos/Adaptador.java
+++ app/src/main/java/com/example/ejemplos/Adaptador.java
@@ -1,5 +1,6 @@
 package com.example.ejemplos;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,8 +8,18 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.proyectoapp.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolder> {
@@ -32,6 +43,7 @@ public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolder> {
     public void onBindViewHolder(ViewHolder holder, int position) {
         String nombre = listaClientes.get(position).getNombre();
         holder.txtnombre.setText(nombre);
+        loadPokemon();
     }
 
     @Override
@@ -45,6 +57,39 @@ public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolder> {
             super(v);
             txtnombre = (TextView) v.findViewById(R.id.txtnombre);
         }
+    }
+    private List<Cliente> pokemon = new ArrayList<>();
+    private RequestQueue requestQueue;
+
+    public void loadPokemon() {
+        String url = "https://pokeapi.co/api/v2/pokemon?limit=151";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, (String) null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray results = response.getJSONArray("results");
+                            for (int i = 0; i < results.length(); i++) {
+                                JSONObject result = results.getJSONObject(i);
+                                pokemon.add(new Cliente(
+                                        result.getString("nombre"),
+                                        result.getString("habilidad"),
+                                        result.getString("tipo")));
+                            }
+
+                            notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            Log.e("cs50", "onResponse: ", e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("cs50", "onErrorResponse: pokemon list error");
+            }
+        });
+        requestQueue.add(request);
+        loadPokemon();
     }
 
 }
